b1ad291128eddb5047b52f8fc51069cb91d7b2aa
epsonagunda <epsonagunda182@gmail.com>
2024-03-12 08:04:52 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-for k in range(0, 10):
-    for l in range(k + 1, 10):
-        if k == 8 and l == 9:
-            print("89")
-        else:
-            print("{}{}, ".format (k, l), end="")
