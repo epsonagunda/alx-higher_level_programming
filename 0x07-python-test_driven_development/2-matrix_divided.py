138d7fb38054fd9d30e48a0db777dd66546c0b22
epsonagunda <epsonagunda182@gmail.com>
2024-04-10 04:04:37 -0700

    matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+a function that divides all elements of a matrix.
+"""
+
 def matrix_divided(matrix, div):
     errorMessage = "matrix must be a matrix (list of lists) of integers/floats"
     if not matrix:
