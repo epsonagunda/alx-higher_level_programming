d677712e6d9a4bf62c152d1924c08e084065386e
epsonagunda <epsonagunda182@gmail.com>
2024-04-10 03:27:04 -0700

    print -square

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Print Square module, for printing squares with "#".
+
+useful for all square-based applications
+"""
 def print_square(size):
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
