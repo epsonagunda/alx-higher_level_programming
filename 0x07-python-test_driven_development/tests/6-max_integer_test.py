38f51b95cb5148239ddcbb22d9f143893939616e
epsonagunda <epsonagunda182@gmail.com>
2024-04-10 03:16:49 -0700

    max_integer

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Here is a short script to 
+test max_integer([..])"""
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+# A testcase is created by subclassing unittest.TestCase
+class TestMaxInteger(unittest.TestCase):
+
+    # All tests you make must be passable by the function below
+    # The unittest module provides tools for constructing and running tests
+    # test_ means it's a method
+    def test_upper(self):
+        self.assertEqual(max_integer([6, 7, 8, 9]), 9)
+
+    def test_none(self):
+        self.assertEqual(max_integer([]), None)
+
+    def test_one(self):
+        self.assertEqual(max_integer([2]), 2)
+
+    def test_one_neg(self):
+        self.assertEqual(max_integer([-10]), -10)
+
+    def test_neg(self):
+        self.assertEqual(max_integer([1, -2, -3, -4]), 1)
+
+    def test_middle(self):
+        self.assertEqual(max_integer([1, 3, 8, 2, 6]), 8)
+
+if __name__ == '__main__':
+    unittest.main()
