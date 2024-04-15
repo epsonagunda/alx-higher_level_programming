12c088bbff41ed37b14f4a9850e155adaa19a70c
epsonagunda <epsonagunda182@gmail.com>
2024-04-15 06:14:01 -0700

    inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-'''
-function that returns the list of available attributes and methods of an object
-'''
+"""
+=============================
+Module with the method lookpu
+=============================
+"""
+
+
 def lookup(obj):
+    """Function for return the attributes for an object"""
+
     return dir(obj)
