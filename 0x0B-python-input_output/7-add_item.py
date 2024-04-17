6e602c5ee425cdb16b1efa3dc62947129c3056af
epsonagunda <epsonagunda182@gmail.com>
2024-04-17 08:39:43 -0700

    load,add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """a script that adds all arguments to a Python list"""
 import sys
-import json
+
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if __name__ == "__main__":
-    try:
-        json_list = load_from_json_file('add_item.json')
-    except FileNotFoundError:
-        json_list = []
+arglist = list(sys.argv[1:])
 
-    for i in range(1, len(sys.argv)):
-        json_list.append(sys.argv[i])
-    save_to_json_file(json_list, "add_item.json")
+    try:
+        old_data = load_from_json_file('add_item.json')
+    except Exception:
+        old_data = []
+
+old_data.extend(arglist)
+save_to_json_file(old_data, "add_item.json")
