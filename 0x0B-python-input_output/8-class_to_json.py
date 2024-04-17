3574991b13febdb42f49da3a1da5507591a576fe
epsonagunda <epsonagunda182@gmail.com>
2024-04-17 09:02:56 -0700

    Class to JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,8 +1,6 @@
 !/usr/bin/python3
 """define class_to_json function"""
-
 def class_to_json(obj):
     """ function that returns the dictionary
-    description
-    """
-    return obj._dict_
+    description with simple data structure"""
+    return vars(obj)
