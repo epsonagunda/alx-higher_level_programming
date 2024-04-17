6d2f2be172a9ddc7806ef43d3350b7c30a055410
epsonagunda <epsonagunda182@gmail.com>
2024-04-17 07:38:35 -0700

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
+"""a function that reads a text file (UTF8) and prints it to stdout """
 
 def read_file(filename=""):
+    """reads filename with utf-8"""
     with open(filename, "r", encoding="UTF-8") as f:
         print(f.read(), end="")
