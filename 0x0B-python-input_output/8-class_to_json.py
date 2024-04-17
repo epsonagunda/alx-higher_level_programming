2ba6c70e4c49392a818bff734ea805d0cf57839f
epsonagunda <epsonagunda182@gmail.com>
2024-04-17 08:48:52 -0700

    class

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -5,7 +5,4 @@ def class_to_json(obj):
     """ function that returns the dictionary
     description
     """
-    dic = {}
-    if hasattr(obj, "__dict__"):
-        dic = obj.__dict__.copy()
-    return dic
+    return obj._dict_
