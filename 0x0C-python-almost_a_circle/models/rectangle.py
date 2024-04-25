7a4201d332f0a01605dad2b6d94fe97a43989eec
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 <epsonagunda182@gmail.com>
2024-04-25 08:08:11 -0700

    rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""
+class rectangel that inherits from base
+"""
+from models.base import Base
+
+class Rectangle(Base):
+    """
+    rectangle class
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
+if __name__ == "_main_":
+
+    r1 = Rectangle(10, 2)
+    print(r1.id)
+
+    r2 = Rectangle(2, 10)
+    print(r2.id)
+
+    r3 = Rectangle(10, 2, 0, 0, 12)
+    print(r3.id)
