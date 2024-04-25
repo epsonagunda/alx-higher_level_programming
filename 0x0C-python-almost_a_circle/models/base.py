caec0beae568dc6e3ac61ddb26b0b0612ee4388f
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 <epsonagunda182@gmail.com>
2024-04-25 00:34:45 -0700

    base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+define model base class
+"""
+class Base:
+    """represent base class"""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+if __name__ == "__main__":
+
+    b1 = Base()
+    print(b1.id)
+
+    b2 = Base()
+    print(b2.id)
+
+    b3 = Base()
+    print(b3.id)
+
+    b4 = Base(12)
+    print(b4.id)
+
+    b5 = Base()
+    print(b5.id)
+
