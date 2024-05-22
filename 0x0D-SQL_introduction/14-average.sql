de26a57e163578c9a68a7a328c78367883309364
epsonagunda <epsonagunda182@gmail.com>
2024-05-22 01:15:22 -0700

    sql introduction

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,2 @@
--- Select count and grouping
--- Execute: cat 15-groups.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+-- Script that computes the score average of all records in the table
 SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
