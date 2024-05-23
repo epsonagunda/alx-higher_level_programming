218345ca60a0c4817193907c7a5a0ebadd4acb98
epsonagunda <epsonagunda182@gmail.com>
2024-05-23 05:52:49 -0700

    more sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,9 +1,10 @@
--- lists all shows and linked genres from the database hbtn_0d_tvshows
-   -- If a show doesn’t have a genre, display NULL in the genre column
-   -- Each record should display:
-      -- tv_shows.title - tv_genres.name
-   -- Results must be sorted in ascending order by the show title and genre
-   -- The database name will be passed as an argument of the mysql command
+-- script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+-- If a show doesn’t have a genre, display NULL in the genre column
+-- Each record should display: tv_shows.title - tv_genres.name
+-- Results must be sorted in ascending order by the show title and genre name
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+
 
 SELECT title, tv_genres.name FROM tv_shows
 LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
