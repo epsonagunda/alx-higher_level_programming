afd4a9a4b6f3a1700a9b036d8724fbe03753e4a2
epson agunda <epsonagunda182@gmail.com>
2024-06-21 23:31:41 -0700

    filter state

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+from sys import argv
+
+# The code should not be executed when imported
+if __name__ == '__main__':
+    # connect to the MYSQL server running on localhost at port 3306
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+
+    # create a cursor object
+    cur = db.cursor()
+    #execute the SQL query
+    cur.execute("SELECT * FROM states WHERE name\
+                LIKE BINARY 'N%' ORDER BY id ASC")
+
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    # close the cursor and the connection
+    cur.close()
+    db.close()
