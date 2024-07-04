9f7c1fa235c4ab0183e857521a69b6204f0652e3
epsonagunda <epsonagunda182@gmail.com>
2024-07-04 06:58:37 -0700

    python network

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# script that takes in a URL, sends a GET request to the URL, and
-#displays the body of the response
-curl -sX GET $1 -L
+# script that takes in a URL, sends a GET request to the URL
+if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -Ls "$1"; fi
